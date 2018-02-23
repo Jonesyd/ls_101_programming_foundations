2488218ae87b07da69beed0216644ed57b2c5e97
Craig Jones <craigjonesy@hotmail.com>
2018-02-23 18:52:59 +1100

    after refactor

--- 02_lessons/21_rps_bonus_features.rb
+++ 02_lessons/21_rps_bonus_features.rb
@@ -1,14 +1,15 @@
+RULE = {
+  rock: ["scissors", "lizard"],
+  paper: ["rock", "sprock"],
+  scissors: ["paper", "lizard"],
+  lizard: ["sprock", "paper"],
+  sprock: ["scissors", "rock"]
+}
+
+WIN = 5
+
 def win?(first, second)
-  (first == "rock" && second == "scissors") ||
-    (first == "rock" &&  second == "lizard") ||
-    (first == "paper" && second == "rock") ||
-    (first == "paper" && second == "sprock") ||
-    (first == "scissors" && second == "paper") ||
-    (first == "scissors" && second == "lizard") ||
-    (first == "lizard" && second == "sprock") ||
-    (first == "lizard" && second == "paper") ||
-    (first == "sprock" && second == "scissors") ||
-    (first == "sprock" && second == "rock")
+  RULE[first.to_sym].include?(second)
 end
 
 def display_results(player, computer)
@@ -22,9 +23,9 @@ def display_results(player, computer)
 end
 
 def display_score(player, computer)
-  if player == 5
+  if player == WIN
     prompt "You are the GRAND CHAMPION!!!"
-  elsif computer == 5
+  elsif computer == WIN
     prompt "The computer is the GRAND CHAMPION!!!"
   else
     prompt "Score: You = #{player}, Computer = #{computer}"
@@ -36,8 +37,12 @@ def prompt(message)
   Kernel.puts("=> #{message}")
 end
 
-thing = { r: "rock", p: "paper", s: "scissors", l: "lizard", k: "sprock" }
-20
+def clear
+  system("clear") || system("cls")
+end
+
+weapon = { r: "rock", p: "paper", s: "scissors", l: "lizard", k: "sprock" }
+
 loop do
   player_score = 0
   computer_score = 0
@@ -47,46 +52,38 @@ loop do
 
     loop do
       prompt("Enter a single letter to make your choice: ")
-      thing.each { |k, v| puts "     #{k}  (for #{v})" }
-      choice = Kernel.gets().chomp().to_sym
-
-      if thing.key?(choice)
-        break
-      else
-        prompt("That's not a valid choice.")
-      end
+      weapon.each { |k, v| puts "     #{k}  (for #{v})" }
+      choice = Kernel.gets().chomp().downcase.to_sym
+      weapon.key?(choice) ? break : prompt("That's not a valid choice.")
     end
 
-    computer_choice = thing.values.sample
+    computer_choice = weapon.values.sample
 
-    prompt("You chose: #{thing[choice]}. Computer chose: #{computer_choice}")
-    display_results(thing[choice], computer_choice)
+    clear
+    prompt("You chose: #{weapon[choice]}. Computer chose: #{computer_choice}")
+    display_results(weapon[choice], computer_choice)
 
-    if win?(thing[choice], computer_choice)
+    if win?(weapon[choice], computer_choice)
       player_score += 1
-    elsif win?(computer_choice, thing[choice])
+    elsif win?(computer_choice, weapon[choice])
       computer_score += 1
     end
 
     display_score(player_score, computer_score)
-    player_score == 5 || computer_score == 5 ? break : prompt("Go Again!!!")
+    player_score == WIN || computer_score == WIN ? break : prompt("Go Again!!!")
     puts("")
   end
 
-  another_game = nil
+  again = nil
   loop do
     prompt("Do you want to play again? 'Y' or 'N'")
-    another_game = Kernel.gets().chomp().upcase
-    if ["N", "Y"].include?(another_game)
-      break
-    else
-      prompt("Just a simple 'Y' or 'N' thanks!")
-    end
+    again = Kernel.gets().chomp().upcase
+    ["N", "Y"].include?(again) ? break : prompt("Just 'Y' or 'N' thanks!")
   end
 
-  break unless another_game == "Y"
-  system("clear") || system("cls")
+  break unless again == "Y"
+  clear
 end
 
-system("clear") || system("cls")
+clear
 prompt("Thank you for playing. Good Bye!")
